74284787ad1c65db76274adcf1a6b9bcf487ae03
HaojunRen <1394997@qq.com>
2020-11-13 11:05:46 +0800

    重构界面

--- desktop/src/main/java/com/nepxion/discovery/console/desktop/ConsoleHierarchy.java
+++ desktop/src/main/java/com/nepxion/discovery/console/desktop/ConsoleHierarchy.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.nepxion.discovery.console.desktop.common.component.AbstractConsoleHierarchy;
-import com.nepxion.discovery.console.desktop.common.context.UIContext;
+import com.nepxion.discovery.console.desktop.common.context.ConsoleUIContext;
 import com.nepxion.discovery.console.desktop.common.icon.ConsoleIconFactory;
 import com.nepxion.discovery.console.desktop.common.locale.ConsoleLocaleFactory;
 import com.nepxion.discovery.console.desktop.workspace.toggle.ConsoleToggleConstant;
@@ -37,7 +37,7 @@ public class ConsoleHierarchy extends AbstractConsoleHierarchy {
         JToggleList list = createList(elementNodes);
         list.setSelectedIndex(0);
 
-        JShrinkOutlook shrinkOutlook = shrinkOutlookBar.addShrinkOutlook(ConsoleLocaleFactory.getString("service_control"), ConsoleIconFactory.getSwingIcon("stereo/favorite_16.png"), ConsoleIconFactory.getSwingIcon("stereo/favorite_add_16.png"), ConsoleLocaleFactory.getString("service_control"), new Font(UIContext.getFontName(), Font.BOLD, UIContext.getMiddleFontSize()));
+        JShrinkOutlook shrinkOutlook = shrinkOutlookBar.addShrinkOutlook(ConsoleLocaleFactory.getString("service_control"), ConsoleIconFactory.getSwingIcon("stereo/favorite_16.png"), ConsoleIconFactory.getSwingIcon("stereo/favorite_add_16.png"), ConsoleLocaleFactory.getString("service_control"), new Font(ConsoleUIContext.getFontName(), Font.BOLD, ConsoleUIContext.getMiddleFontSize()));
         shrinkOutlook.setContentPane(list);
         shrinkOutlook.addPropertyChangeListener(new OutlookSelectionListener());
 
