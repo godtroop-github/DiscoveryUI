48fc287b0f66f7ccfd5923c174bdbdc11ccd8719
兰健 <790115264@qq.com>
2021-01-08 11:21:44 +0800

    全链路服务蓝绿 页面

---
+++ web/src/pages/servicesDeployManager/serviceBlueGreen/add/index.jsx
@@ -0,0 +1,146 @@
+import { Button, Result, Space, Radio, Modal, Divider, Row, Col, Typography, Menu, Dropdown, Tabs, Input } from 'antd';
+import React, { useState } from 'react';
+import { history } from 'umi';
+import { PlusOutlined, DownOutlined } from '@ant-design/icons';
+import { constant } from 'lodash';
+const { Title, Text, Link } = Typography;
+
+
+const subscribeGroupMenu = (
+ <Menu>
+  <Menu.Item key="0">
+   <a href="http://www.alipay.com/">1st menu item</a>
+  </Menu.Item>
+  <Menu.Item key="1">
+   <a href="http://www.taobao.com/">2nd menu item</a>
+  </Menu.Item>
+  <Menu.Divider />
+  <Menu.Item key="3">3rd menu item</Menu.Item>
+ </Menu>
+)
+
+const subscribeServiceMenu = (
+ <Menu>
+  <Menu.Item key="0">
+   <a href="http://www.alipay.com/">1st menu item</a>
+  </Menu.Item>
+  <Menu.Item key="1">
+   <a href="http://www.taobao.com/">2nd menu item</a>
+  </Menu.Item>
+  <Menu.Divider />
+  <Menu.Item key="3">3rd menu item</Menu.Item>
+ </Menu>
+)
+
+
+
+const serviceBlueGreenNew = () => {
+ const [visible, setVisible] = useState(false);
+ // 新建 - 确定
+ const addSubmit = () => {
+  setVisible(false);
+ }
+
+ return (
+  <>
+     <Space>
+      <Button icon={<PlusOutlined />}
+       onClick={() => {
+        setVisible(true);
+       }}
+      >新增</Button>
+     </Space>
+     
+
+   <Modal visible={visible}
+    onOk={addSubmit}
+    onCancel={() => {
+     setVisible(false);
+    }}
+   >
+    <Divider orientation="left">订阅参数</Divider>
+
+    <Row gutter={[16, 16]}>
+     <Col flex="40px">
+     </Col>
+     <Col flex="100px">
+      <Text>订阅类型</Text>
+     </Col>
+     <Col auto>
+      <Radio.Group name="radiogroup" defaultValue={1}>
+       <Radio value={1}>局部订阅</Radio>
+       <Radio value={2}>全局订阅</Radio>
+      </Radio.Group>
+     </Col>
+    </Row>
+    <Row align="middle" gutter={[16, 16]}>
+     <Col flex="40px">
+     </Col>
+     <Col flex="100px">
+      <Text>订阅组名</Text>
+     </Col>
+     <Col auto>
+      <Dropdown overlay={subscribeGroupMenu} placement="bottomLeft" arrow>
+       <Button>请选择要订阅的组名 <DownOutlined /></Button>
+      </Dropdown>
+     </Col>
+    </Row>
+    <Row align="middle" gutter={[16, 16]}>
+     <Col flex="40px">
+     </Col>
+     <Col flex="100px">
+      <Text>订阅服务名</Text>
+     </Col>
+     <Col auto>
+      <Dropdown overlay={subscribeServiceMenu} placement="bottomLeft" arrow>
+       <Button>请选择要订阅的服务名 <DownOutlined /></Button>
+      </Dropdown>
+     </Col>
+    </Row>
+    <Divider orientation="left">部署参数</Divider>
+    <Row gutter={[16, 16]}>
+     <Col flex="40px">
+     </Col>
+     <Col flex="100px">
+      <Text>部署模式</Text>
+     </Col>
+     <Col auto>
+      <Radio.Group name="radiogroup" defaultValue={1}>
+       <Radio value={1}>域网管模式</Radio>
+       <Radio value={2}>非域网关模式</Radio>
+      </Radio.Group>
+     </Col>
+    </Row>
+    <Divider orientation="left">发布参数</Divider>
+    <Row gutter={[16, 16]}>
+     <Col flex="40px">
+     </Col>
+     <Col flex="100px">
+      <Text>发布策略</Text>
+     </Col>
+     <Col auto>
+      <Radio.Group name="radiogroup" defaultValue={1}>
+       <Radio value={1}>版本策略</Radio>
+       <Radio value={2}>区域策略</Radio>
+      </Radio.Group>
+     </Col>
+    </Row>
+    <Row gutter={[16, 16]}>
+     <Col flex="40px">
+     </Col>
+     <Col flex="100px">
+      <Text>路由类型</Text>
+     </Col>
+     <Col auto>
+      <Radio.Group name="radiogroup" defaultValue={1}>
+       <Radio value={1}>蓝|绿|兜底</Radio>
+       <Radio value={2}>蓝|兜底</Radio>
+      </Radio.Group>
+     </Col>
+    </Row>
+   </Modal>
+  </>
+ );
+};
+
+export default serviceBlueGreenNew;
--- web/src/pages/servicesDeployManager/serviceBlueGreen/index.jsx
+++ web/src/pages/servicesDeployManager/serviceBlueGreen/index.jsx
@@ -1,9 +1,71 @@
-import { Button, Result } from 'antd';
-import React from 'react';
+import { Button, Result, Space, Radio, Modal, Divider, Row, Col, Typography, Tabs, Input, Select } from 'antd';
+import React, { useState } from 'react';
 import { history } from 'umi';
+import { PlusOutlined, DownOutlined } from '@ant-design/icons';
+import { constant } from 'lodash';
+const { TabPane } = Tabs;
 
-const serviceBlueGreen = () => (
-  <div>serviceBlueGreen</div>
-);
+const { Option } = Select;
+
+import ServiceBlueGreenNew from './add/index.jsx';
+
+const serviceBlueGreen = () => {
+
+  return (
+    <>
+      <Row>
+        <Col flex={10}><ServiceBlueGreenNew></ServiceBlueGreenNew></Col>
+        <Col flex={3}>
+          <Divider orientation="left">蓝绿条件</Divider>
+          <Tabs defaultActiveKey="1" >
+            <TabPane tab="蓝" key="1">
+              <Row>
+                <Col flex={1}>
+                  参数
+              </Col>
+                <Col flex={1}>运算</Col>
+                <Col flex={1}>值</Col>
+                <Col flex={1}>关系</Col>
+              </Row>
+              <Row>
+                <Col flex={1}>
+                  <Input placeholder="Basic usage" />
+                </Col>
+                <Col flex={1}>
+                  <Select defaultValue="lucy" style={{ width: 120 }}>
+                    <Option value="jack">==</Option>
+                    <Option value="lucy">!=</Option>
+                    <Option value="disabled">
+                      &gt;
+                  </Option>
+                    <Option value="Yiminghe">
+                      &gt;=
+                  </Option>
+                    <Option value="Yiminghe">
+                      &lt;
+                  </Option>
+                    <Option value="Yiminghe">
+                      &lt;=
+                  </Option>
+                    <Option value="Yiminghe">
+                      match
+                  </Option>
+                  </Select>
+                </Col>
+                <Col flex={1}>
+                  <Input placeholder="Basic usage" />
+                </Col>
+                <Col flex={1}>关系</Col>
+              </Row>
+            </TabPane>
+            <TabPane tab="绿" key="2">
+              Content of Tab Pane 2
+            </TabPane>
+          </Tabs>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export default serviceBlueGreen;
